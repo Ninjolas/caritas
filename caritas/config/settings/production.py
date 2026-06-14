6b30ba8bd7273b841a434f47872d1a3155f4420e
Nicolas Grillo Groff <nicolasgg04@gmail.com>
2026-06-14 17:00:11 -0300

    chore: add Render deploy configuration

--- caritas/config/settings/production.py
+++ caritas/config/settings/production.py
@@ -1,4 +1,5 @@
 from .base import *
+import dj_database_url
 from decouple import config
 
 DEBUG = False
@@ -7,13 +8,23 @@ SECRET_KEY = config('SECRET_KEY')
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
 
+# Banco de dados via DATABASE_URL (fornecido automaticamente pelo Render)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT': config('DB_PORT', default='5432'),
-    }
+    'default': dj_database_url.config(
+        env='DATABASE_URL',
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
+
+# Arquivos estáticos com WhiteNoise
+MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Segurança
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
